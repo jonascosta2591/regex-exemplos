2ffb6795e258946ed4fbe110aaf95e01c68faec4
jonas lima <jonascosta0299@gmail.com>
2023-12-24 17:49:26 -0300

    regex exemplos

---
+++ index.js
@@ -0,0 +1,16 @@
+const text = 'marcos santos vieira'
+
+console.log(text.search(/santos/g))
+
+console.log(text.match(/o/g))
+
+// console.log(text.replace(/santos/i, 'lima'))
+
+// console.log(text.replace(/o/i, '0'))
+
+// Metacaracteres
+// console.log(text.match(/[a-z|0-9]/ig))
+// console.log(text.match(/\d/ig)) Números
+// console.log(text.match(/\s/ig)) Espaços
+// console.log(text.match(/\bc/ig))
+
